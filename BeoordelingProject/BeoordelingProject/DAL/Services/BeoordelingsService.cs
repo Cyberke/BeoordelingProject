fb1ddf8b616f990590cc763896ce24fcab887f6a
Manuel Dellisse <manuel.dellisse@student.howest.be>
2014-06-04 16:50:13 +0200

    Starten met het implementeren van de engine.
    BeoordelingsService (niet volledig),
    BeoordelingsVM (moet misschien aan gepast worden),
    BeoordelaarController (zo goed als leeg),
    View (moet nog aangemaakt worden)
    
    Momenteel moet er een paar problemen opgelost worden.

---
+++ BeoordelingProject/BeoordelingProject/DAL/Services/BeoordelingsService.cs
@@ -0,0 +1,42 @@
+﻿using BeoordelingProject.DAL.Repositories;
+using BeoordelingProject.DAL.UnitOfWork;
+using BeoordelingProject.Engine;
+using BeoordelingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeoordelingProject.DAL.Services {
+    public class BeoordelingsService : BeoordelingProject.DAL.Services.IBeoordelingsService {
+        IUnitOfWork uow = null;
+        IGenericRepository<Matrix> beoordelingsRepository = null;
+        IGenericRepository<Resultaat> resultaatRepository = null;
+        IBeoordelingsEngine beoordelingsEngine = null;
+
+        public BeoordelingsService() {
+        }
+
+        public BeoordelingsService(IUnitOfWork uow,
+            IGenericRepository<Matrix> beoordelingsRepository,
+            IGenericRepository<Resultaat> resultaatRepository,
+            IBeoordelingsEngine beoordelingsEngine) {
+                this.uow = uow;
+                this.beoordelingsRepository = beoordelingsRepository;
+                this.resultaatRepository = resultaatRepository;
+                this.beoordelingsEngine = beoordelingsEngine;
+        }
+
+        public List<Resultaat> GetResultaten() {
+            return resultaatRepository.All().ToList<Resultaat>();
+        }
+
+        public Matrix GetMatrix() {
+            return beoordelingsRepository.All().ToList<Matrix>()[0];
+        }
+
+        public void CreateBeoordeling() {
+            //
+        }
+    }
+}
---
+++ BeoordelingProject/BeoordelingProject/DAL/Services/IBeoordelingsService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace BeoordelingProject.DAL.Services {
+    public interface IBeoordelingsService {
+        void CreateBeoordeling();
+        BeoordelingProject.Models.Matrix GetMatrix();
+        System.Collections.Generic.List<BeoordelingProject.Models.Resultaat> GetResultaten();
+    }
+}
