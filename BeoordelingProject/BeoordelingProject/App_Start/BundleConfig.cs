07f4586560e0d2e5e0ba2efa650d3f7b067eb0e1
Cyberke <jelle1vdb@hotmail.com>
2014-06-04 16:00:29 +0200

    Registreren & login fixen

--- BeoordelingProject/BeoordelingProject/App_Start/BundleConfig.cs
+++ BeoordelingProject/BeoordelingProject/App_Start/BundleConfig.cs
@@ -25,7 +25,13 @@ namespace BeoordelingProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/javascripts").Include(
+                "~/Scripts/JSfuncties.js"));
         }
+
+            
     }
 }
