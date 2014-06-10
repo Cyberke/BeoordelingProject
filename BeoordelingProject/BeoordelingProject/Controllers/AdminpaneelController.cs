32d24ba300f6e167b2db215b4f218351fdf745f4
Manuel Dellisse <manuel.dellisse@student.howest.be>
2014-06-10 15:16:47 +0200

    Starten met adminpaneel. Moest commiten omdat er veranderingen zijn in de database die ik nodig heb.
    Todo: Email, wachtwoord en automatisch feedback opslaan.

---
+++ BeoordelingProject/BeoordelingProject/Controllers/AdminpaneelController.cs
@@ -0,0 +1,40 @@
+﻿using BeoordelingProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeoordelingProject.Controllers
+{
+    public class AdminpaneelController : Controller
+    {
+        //
+        // GET: /Adminpaneel/
+        public ActionResult Index()
+        {
+            AdminpaneelVM vm = new AdminpaneelVM();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(AdminpaneelVM vm, string adminpaneelButtons) {
+            if (adminpaneelButtons.Equals("Opslaan")) {
+                var email = vm.Email;
+                var wachtwoord = vm.WachtwoordVM.NewPassword;
+                var autoFeedback = vm.AutoFeedback;
+
+                // Service om email, wachtwoord en auto feedback te wijzigen
+            }
+            else if (adminpaneelButtons.Equals("Studenten importeren")) {
+                return RedirectToAction("Index", "Student");
+            }
+            else {
+                return RedirectToAction("AddStudentRol", "Accountbeheer");
+            }
+
+            return RedirectToAction("Index");
+        }
+	}
+}
---
+++ BeoordelingProject/BeoordelingProject/ViewModel/AdminpaneelVM.cs
@@ -0,0 +1,16 @@
+﻿using BeoordelingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BeoordelingProject.ViewModel {
+    public class AdminpaneelVM {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public ManageUserViewModel WachtwoordVM { get; set; }
+        public bool AutoFeedback { get; set; }
+    }
+}
