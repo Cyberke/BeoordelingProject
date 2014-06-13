cdc0952d87743ae8632fa99c051e8134d3c4cdb6
Milan Tocan <milan.tocan@gmail.com>
2014-06-13 09:10:05 +0200

    flurp

--- BeoordelingProject/BeoordelingProject/DAL/Services/IStudentrolService.cs
+++ BeoordelingProject/BeoordelingProject/DAL/Services/IStudentrolService.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-namespace BeoordelingProject.DAL.Services {
-    public interface IStudentrolService {
-        BeoordelingProject.Models.StudentRollen CreateStudentrol(BeoordelingProject.Models.Student student, BeoordelingProject.Models.Rol rol);
+namespace BeoordelingProject.DAL.Services
+{
+    public interface IStudentrolService
+    {
         BeoordelingProject.Models.StudentRollen CreateStudentrol(BeoordelingProject.Models.Student student, System.Collections.Generic.List<BeoordelingProject.Models.Rol> rollen);
     }
 }
