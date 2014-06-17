7111f04bcfce6be815c8277f91b9e0c4e5daedfc
Milan Tocan <milan.tocan@gmail.com>
2014-06-17 23:56:09 +0200

    matrix wijzigen
    todo: tabel wat stylen, list<int>rollen "parsen" en terug in db zetten

---
+++ BeoordelingProject/BeoordelingProject/Controllers/MatrixController.cs
@@ -0,0 +1,53 @@
+﻿using BeoordelingProject.DAL.Services;
+using BeoordelingProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeoordelingProject.Controllers
+{
+    public class MatrixController : Controller
+    {
+        private MatrixbeheerService matrixbeheerservice = null;
+        public MatrixController()
+        {
+
+        }
+        public MatrixController(MatrixbeheerService matrixbeheerservice)
+        {
+            this.matrixbeheerservice = matrixbeheerservice;
+        }
+        //
+        // GET: /Matrix/
+        public ActionResult EditMatrix(string opleiding, bool tussentijds = false)
+        {
+            if (opleiding != null && !opleiding.Equals("") )
+            {
+                MatrixbeheerVM vm = new MatrixbeheerVM();
+                vm.Matrix = matrixbeheerservice.GetMatrixByRichtingByTussentijds(opleiding, tussentijds);
+
+                if (vm.Matrix != null)
+                    return View(vm);
+                else
+                    return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult EditMatrix()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            MatrixbeheerVM vm = new MatrixbeheerVM();
+            vm.Opleidingen = matrixbeheerservice.GetOpleidingen();
+            return View(vm);
+        }
+	}
+}
---
+++ BeoordelingProject/BeoordelingProject/DAL/Services/IMatrixbeheerService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BeoordelingProject.DAL.Services
+{
+    public interface IMatrixbeheerService
+    {
+        BeoordelingProject.Models.Matrix GetMatrixByRichtingByTussentijds(string opleiding, bool tussentijds);
+        System.Collections.Generic.List<string> GetOpleidingen();
+    }
+}
