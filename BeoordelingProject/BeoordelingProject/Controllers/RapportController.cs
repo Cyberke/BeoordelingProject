6d0eef1900f6da1158ec0b17b213be407963e271
PeterVdK <peter.vandenkerckhove@student.howest.be>
2014-06-18 12:53:25 +0200

    Hopelijk werkt diene pdf shizzle

---
+++ BeoordelingProject/BeoordelingProject/Controllers/RapportController.cs
@@ -0,0 +1,27 @@
+﻿using BeoordelingProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeoordelingProject.Controllers
+{
+    public class RapportController : Controller
+    {
+        //
+        // GET: /Rapport/
+        public ActionResult Index()
+        {
+            RapportVM rapport = new RapportVM
+            {
+                Academiejaar = "2014-2015",
+                Naam = "Peter VandenKerckhove",
+                Richting = "BaKo",
+                Punt = 18
+            };
+
+            return new RazorPDF.PdfResult(rapport, "Index");
+        }
+	}
+}
---
+++ BeoordelingProject/BeoordelingProject/ViewModel/RapportVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeoordelingProject.ViewModel
+{
+    public class RapportVM
+    {
+        public string Naam { get; set; }
+        public string Richting { get; set; }
+        public string Academiejaar { get; set; }
+        public int Punt { get; set; }
+    }
+}
